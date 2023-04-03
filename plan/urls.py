aaff6cd1ec87bfc5ace705d2e40ff4547af42c45
BigMcQueen <mmmrym10@gmail.com>
2023-04-03 23:33:34 +0900

    Add planDeleteFunction

--- plan/urls.py
+++ plan/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', PlanList.as_view(), name='list'),
     path('detail/<int:pk>', PlanDetail.as_view(), name='detail'),
     path('create/', PlanCreate.as_view(), name='create'),
+    path('delete/<int:pk>', PlanDelete.as_view(), name='delete'),
 ]
--- plan/views.py
+++ plan/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView ,DetailView, CreateView
+from django.views.generic import ListView ,DetailView, CreateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import *
@@ -9,6 +9,9 @@ class PlanList(ListView):
     model = Plan
     context_object_name = 'plans'
 
+    def get_queryset(self):
+        return Plan.objects.order_by('scheduled_date')
+
 class PlanDetail(DetailView):
     model = Plan
     context_object_name = 'plan'
@@ -16,4 +19,9 @@ class PlanDetail(DetailView):
 class PlanCreate(CreateView):
     model = Plan
     form_class = PlanForm
+    success_url = reverse_lazy('list')
+
+class PlanDelete(DeleteView):
+    model = Plan
+    context_object_name = 'plan'
     success_url = reverse_lazy('list')
