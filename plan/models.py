0df23d8c741446bc4371582f477e673443e89920
BigMcQueen <mmmrym10@gmail.com>
2023-04-05 00:53:34 +0900

    Change models

--- plan/models.py
+++ plan/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Friend(models.Model):
+    email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.email
+
 class Plan(models.Model):
+    friend = models.ManyToManyField(Friend, related_name='friend')
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -12,15 +19,3 @@ class Plan(models.Model):
 
     def __str__(self):
         return self.title
-
-class Friends(models.Model):
-
-    class Meta:
-        verbose_name = '友達リスト'
-        verbose_name_plural = '友達リスト'
-
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'user_friends')
-    friend = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='友達', related_name = 'friend_friends')
-
-    def __str__(self):
-        return f'{self.friend}'
