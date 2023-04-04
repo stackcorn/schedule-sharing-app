2ee483bcff9b722184484d4c10de84b34c33d69c
BigMcQueen <mmmrym10@gmail.com>
2023-04-04 23:36:02 +0900

    migrate

--- plan/models.py
+++ plan/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Plan(models.Model):
@@ -11,3 +12,15 @@ class Plan(models.Model):
 
     def __str__(self):
         return self.title
+
+class Friends(models.Model):
+
+    class Meta:
+        verbose_name = '友達リスト'
+        verbose_name_plural = '友達リスト'
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name = 'user_friends')
+    friend = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='友達', related_name = 'friend_friends')
+
+    def __str__(self):
+        return f'{self.friend}'
