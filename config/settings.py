2eb0f6978fa485588f163fd11cb7aa1e60f42480
BigMcQueen <mmmrym10@gmail.com>
2023-04-09 22:09:58 +0900

    makemigrations

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'account.apps.AccountConfig',
+    'schedule.apps.ScheduleConfig',
     'widget_tweaks',
 ]
 
--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Schedule(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.CharField(max_length=100)
+    scheduled_date = models.DateField()
+
+    def __str__(self):
+        return self.title
